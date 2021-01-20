c5247a63cce4ac09334d81f70374785da86627bd
Astrid Yu <astrid@astrid.tech>
2021-01-20 10:35:04 -0800

    Expand tests

--- src/Board.test.js
+++ src/Board.test.js
@@ -8,6 +8,17 @@ test('should mutate on button click', () => {
     fireEvent.click(buttons[0]);
 
     expect(buttons[0].innerHTML).toEqual('X')
+
+    fireEvent.click(buttons[1]);
+
+    expect(buttons[0].innerHTML).toEqual('X')
+    expect(buttons[1].innerHTML).toEqual('O')
+
+    fireEvent.click(buttons[2]);
+
+    expect(buttons[0].innerHTML).toEqual('X')
+    expect(buttons[1].innerHTML).toEqual('O')
+    expect(buttons[2].innerHTML).toEqual('X')
 });
 
 test('should say who the next player is', () => {
